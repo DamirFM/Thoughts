4171536dec32f7ef39943b69838bad1dbeac75d5
DamirFM <yakupovdamir93@gmail.com>
2024-01-24 20:15:13 -0500

    updated User model

--- models/User.js
+++ models/User.js
@@ -2,32 +2,46 @@ const { Schema, model } = require('mongoose');
 
 // Schema to create User model
 const userSchema = new Schema(
-    {
+  {
     username: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true,
-      },
-    email: {
-        type: String,
-        required: true,
-        max_length: 50,
-      },
-    thoughts: {
-        type: String,
-        required: true,
-        max_length: 50,
-      },
-    friends: [assignmentSchema],
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
     },
-    {
-      toJSON: {
-        getters: true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      required: 'Email address is required',
+      validate: [validateEmail, 'Please fill a valid email address'],
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
       },
-    }
-  );
-  
-  const User = model('user', userSchema);
-  
-  module.exports = User;
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+// Virtual called friendCount that retrieves the length of the user's friends array field on query.
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+// Using mongoose.model() to compile a model based on the schema
+const User = model('user', userSchema);
+
+module.exports = User;
